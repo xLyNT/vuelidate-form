e82897b16bc017f0641e57cc7b6cd84b9368352d
Ilya <sakuru1x1@gmail.com>
2022-01-20 14:36:47 +0300

    Added styles and component.

---
+++ src/components/Popup.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class ="popup" :class="{ 'popup-active' : isActive}">
+        <h3 :style="{backgroundColor:color}">{{heading}}</h3>
+        <p v-if="color !== 'green'"><span :key="field" v-for="field of fieldsToShow">{{fieldsToShow.indexOf(field) === fieldsToShow.length - 1 ? field + '.' : field + ', ' }}</span></p>
+    </div>
+</template>
+
+<script>
+
+export default ({
+    name:'Popup',
+    props:{
+        heading:String,
+        message:String,
+        isActive:Boolean,
+        color:String,
+        fieldsToShow:Object
+    }
+})
+</script>
+<style>
+@import "../css/style.css";
+
+</style>
